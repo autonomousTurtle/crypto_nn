8241fb5ccfa0e08a7e5b2a3e9ace9d46ce9dc551
Aaron C <acrookes330@gmail.com>
2021-08-21 16:54:12 -0400

    commit

--- crypto_rnn_rev0.py
+++ crypto_rnn_rev0.py
@@ -17,7 +17,7 @@ FUTURE_PERIOD_PREDICT = 3 #how many minutes in the future to predict
 RATIO_TO_PREDICT = "LTC-USD" #which crypto to look at
 EPOCHS = 10 #number of passes through the data
 BATCH_SIZE = 64 #how many batches of data to run through the network
-NAME = f"{SEQ_LEN}-SEQ-{FUTURE_PERIOD_PREDICT}-PRED-{int(time.time())}"
+NAME = f"{SEQ_LEN}-SEQ-{FUTURE_PERIOD_PREDICT}-PRED-{int(time.time())}" #to save in tensorboard
 
 ######################### FUNCTIONS #######################
 
@@ -38,16 +38,16 @@ def preprocess_df(df):
 
     for col in df.columns: #go through all the columns 
         if col != "target": #if it is not the target value, normalize (target is already a 1 or 0)
-            df[col] = df[col].pct_change # pct_changes normalzies the different currencies to a %
+            df[col] = df[col].pct_change() # pct_changes normalzies the different currencies to a %
             df.dropna(inplace = True) #throw na if data is missing
-            #df[col] = preprocessing.scale(df[col].values) #scales all the values between 0 and 1, .values ignores the time parameter
+            df[col] = preprocessing.scale(df[col].values) #scales all the values between 0 and 1, .values ignores the time parameter
     
     df.dropna(inplace=True) #clean up again in case we missed one
 
 
     sequential_data = [] #sequential_data is a list that will contain the sequences
     prev_days = deque(maxlen = SEQ_LEN) #deque keeps a list with the length SEQ_LEN and kicks the last value out when a new one comes in 
-
+ 
     for i in df.values: #iterate over the values
         prev_days.append([n for n in i[:-1]]) #grab each of the values not including the last value which is the target
         if len(prev_days) == SEQ_LEN: #make sure we have 60 sequences
@@ -109,6 +109,7 @@ for ratio in ratios:
     else: #else, join the dataframes
         main_df = main_df.join(df) 
 
+main_df.fillna(method="ffill", inplace=True)
 main_df.dropna(inplace=True) #make sure the data is good
 #print(main_df.head()) #check how we did
 
@@ -122,7 +123,7 @@ times = sorted(main_df.index.values) #grab the index (time) in sorted order and 
 last_5pct  = sorted(main_df.index.values)[-int(0.05*len(times))] #take last 5% of timestamps to sort through later
 
 validation_main_df = main_df[(main_df.index >= last_5pct)] #validation data is data in last 5%
-main_df = main_df[(main_df.index <= last_5pct)] #training data is data in first 95%
+main_df = main_df[(main_df.index < last_5pct)] #training data is data in first 95%
 
 
 train_x, train_y = preprocess_df(main_df)
@@ -133,3 +134,48 @@ print(f"train data: {len(train_x)} validation: {len(validation_x)}")
 print(f"Dont buys: {train_y.count(0)}, buys: {train_y.count(1)}")
 print(f"VALIDATION Dont buys: {validation_y.count(0)}, buys: {validation_y.count(1)}")
 
+################### NEURAL NETWORK MODEL ########################3
+
+model = Sequential()
+
+model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(LSTM(128, input_shape=(train_x.shape[1:])))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(32, activation="relu"))
+model.add(Dropout(0.2))
+
+#output layer, binary choice
+model.add(Dense(2, activation="softmax"))
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.001, decay=1e-6)
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer = opt, metrics=['accuracy'])
+
+#callbacks
+tensorboard = TensorBoard(log_dir=f'logs/{NAME}') #tensorboard
+filepath = "RNN_Final-{epoch:02d}-{val_acc:.3f}" #checkpoints
+#heckpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc', verbose=1, save_best_only = True, mode='max'))
+
+#re
+train_x = np.asarray(train_x) 
+train_y = np.asarray(train_y)
+validation_x = np.asarray(validation_x)
+validation_y = np.asarray(validation_y)
+
+history = model.fit(train_x, train_y, batch_size=BATCH_SIZE, epochs=EPOCHS,validation_data=(validation_x, validation_y), callbacks=[tensorboard])#, checkpoint])
+
+#score the model
+score=model.evaluate(validation_x, validation_y, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy', score[1])
+#save model
+model.save(" models/{}".format(NAME))
